cf61f83a620d744503ed7d960ee72f74c415a3c5
Hieu <hohieudn2005@gmail.com>
2025-07-18 22:53:15 +0700

    hieudiy

--- DIY.sql
+++
@@ -1,8 +0,0 @@
--- Thêm cột status để admin có thể bật/tắt bài tuyển dụng
-ALTER TABLE Jobs ADD status NVARCHAR(20) DEFAULT 'active';
-
--- Thêm cột posted_at để lưu ngày đăng bài
-ALTER TABLE Jobs ADD posted_at DATETIME DEFAULT GETDATE();
-
--- Thêm cột gps để lưu lat, lon dưới dạng string
-ALTER TABLE Jobs ADD gps NVARCHAR(50); 
